186e637006ba3134c4fd84d9c33cdb502044c1f7
Lyzander Marciano Andrylie <lyzanderandrylie@gmail.com>
2026-02-13 18:58:18 +0700

    chore: add metadata

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,20 @@
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import Sitemap from "vite-plugin-sitemap";
 import svgr from "vite-plugin-svgr";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(), tsconfigPaths(), svgr()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    tsconfigPaths(),
+    svgr(),
+    Sitemap({
+      hostname: "https://lyzanderandrylie.github.io/portfolio/",
+    }),
+  ],
   base: "/portfolio/",
 });
