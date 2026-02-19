3b08fd9250a6cdfdafc8f439de4229683e7d140f
Lyzander Marciano Andrylie <lyzanderandrylie@gmail.com>
2026-02-19 19:24:48 +0700

    fix: update certification date to 2026

--- src/data/certifications.ts
+++ src/data/certifications.ts
@@ -2,7 +2,7 @@ import type { Certification } from "@/types";
 
 export const CERTIFICATIONS: Certification[] = [
   {
-    date: "Feb 2024",
+    date: "Feb 2026",
     title: "AWS Certified Cloud Practitioner",
     issuer: "Amazon Web Services",
     link: "https://www.credly.com/badges/8e7b4cf2-37d2-4f2d-bc39-93c4a1a02c3e",
